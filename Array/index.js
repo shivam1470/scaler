50c663e74a065d3312075f76c955d3907570cae9
Shivam Mishra <shivammishr16@gmail.com>
2022-12-02 02:48:59 +0530

    Array: contain duplicate

--- Array/index.js
+++ Array/index.js
@@ -1,8 +1,28 @@
-let Array = [2, 3, 5]
+// Intuition
+// store your value to map and if value is already stored then return true
 
-for(let i = 0; i < Array.length; i++){
-    console.log(Array[i])
-}
+// Approach
+// declare a Map and then check if map has than value then return else set to the map until last value and if no value match then return false
 
-// Check you email in commits.
-//Recheck
+// Complexity
+// Time complexity:
+// O(n)
+// Space complexity:
+// O(1)
+
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let map = new Map()
+    for(let i = 0; i < nums.length; i++){
+        if(map.has(nums[i])){
+            return true
+        } else {
+            map.set(nums[i])
+        }
+    }
+    return false
+};
